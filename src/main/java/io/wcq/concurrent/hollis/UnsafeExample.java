d74db68b5bc868e2f2ff728449c998070bd68473
wucq <1289544990@qq.com>
2024-02-01 23:08:57 +0800

    增加unsafe处理类

---
+++ src/main/java/io/wcq/concurrent/hollis/UnsafeExample.java
@@ -0,0 +1,39 @@
+package io.wcq.concurrent.hollis;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class UnsafeExample {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        // 使用放射获取Unsafe实例
+        Field theUnsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) theUnsafeField.get(null);
+
+        // 分配堆外内存，返回内存地址
+        long size = 1024; // 内存大小
+        long address = unsafe.allocateMemory(size);
+
+        // 写入数据到堆外内存
+        String dataToWrite = "Hello, this is hollis testing direct memory!";
+        byte[] dateBytes = dataToWrite.getBytes();
+        for (int i = 0; i < dateBytes.length; i++) {
+            unsafe.putByte(address + i, dateBytes[i]);
+        }
+
+        // 从堆外内存读取数据
+        byte[] dateToRead = new byte[dateBytes.length];
+        for (int i = 0; i < dateBytes.length; i++) {
+            dateToRead[i] = unsafe.getByte(address + i);
+        }
+        System.out.println(new String(dateBytes));
+        System.out.println(new String(dateToRead));
+
+        System.out.println(unsafe.addressSize());
+        // 释放堆外内存
+        unsafe.freeMemory(address);
+        System.out.println(new String(dateBytes));
+        System.out.println(new String(dateToRead));
+    }
+}
